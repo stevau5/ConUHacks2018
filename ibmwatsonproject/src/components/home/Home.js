46552a2cd8e5f2957186b0c887dc46dd108ea43d
tommyb23 <a_balou@live.concordia.ca>
2018-01-27 16:53:46 -0500

    Created frontend boilerplate for react. Added depencies: express, mongoose, body-parser, react-router-dom

---
+++ ibmwatsonproject/src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <body>
+        Home
+      </body>
+    );
+  }
+}
+
+export default Home;
