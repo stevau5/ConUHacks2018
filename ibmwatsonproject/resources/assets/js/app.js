6501b61dd9de4e0b50779988b43bc802c9ebf6a2
tommyb23 <a_balou@live.concordia.ca>
2018-01-27 22:26:45 -0500

    Added .env config in app.js

--- ibmwatsonproject/resources/assets/js/app.js
+++ ibmwatsonproject/resources/assets/js/app.js
@@ -3,10 +3,13 @@ import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
-import ejs from 'ejs';
+
 // CUSTOM FILES
 import apiRoutes from './routes/api';
 import webRoutes from './routes/web';
+
+//.env file
+require('dotenv').config();
 
 // REQUIRE .env FILE
 dotenv.config();
@@ -17,6 +20,9 @@ const app = express();
 // SET UP APP USE
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+// SET VIEW ENGINE
+app.set('view engine', 'ejs');
 
 // Use API routes
 app.use('/api',apiRoutes);
